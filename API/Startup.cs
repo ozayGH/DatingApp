5239cd1ee67660f26488f916c52bb1d036a1d78d
Ozay BAKIRHAN <ozay.bakirhan@horoz.com.tr>
2020-12-04 23:06:19 +0300

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,10 +36,13 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,7 +58,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
